785df9122d3672310c3599af03101ed0fbfdb7f5
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-09-12 16:47:28 +0100

    button displays and changes colour over time

--- Main.cpp
+++ Main.cpp
@@ -2,22 +2,30 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
 	//-----------------------------------------------------------
     //game setup
     //--------------------------------------------------------------
-	//variables
+	
+	
+	//Rendom Window creation
 	sf::RenderWindow gameWindow; //Makes a variable called gameWindow of the type renderwindow
-	gameWindow.create(sf::VideoMode::getDesktopMode(), "Button Masher",
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Quick Draw",
 	sf::Style::Titlebar | sf::Style::Close);
+
+	//seed our random number generation
+	srand(time(NULL));
+
 
 	//Timer functionality
 	float signalTimeLowerLimit = 5.0f;
 	float signalTimerUpperLimit = 10.0f;
 
-	sf::Time timeTilsignal = sf::seconds(0.0f);
+	sf::Time timeTilsignal = sf::seconds(signalTimeLowerLimit);
 	sf::Time timeSinceSignal = sf::seconds(0.0f);
 	sf::Clock gameClock; //game clock
 
@@ -45,43 +53,75 @@ int main()
     //Runs every frame untile the game is closed
 	while (gameWindow.isOpen()) // while continues to loop while the (condition) is true
 	{
-		sf::Event event;
-		while (gameWindow.pollEvent(event))
-		{
+		sf::Event gameEvent;
 
+		while (gameWindow.pollEvent(gameEvent))
+		{
 			//Process events
 
-			if (event.type == sf::Event::Closed)
+
+			//check if event is a mouse button pressed
+			if (gameEvent.type == sf::Event::MouseButtonPressed)
+			{
+				if (buttonSprite.getGlobalBounds().contains(gameEvent.mouseButton.x, gameEvent.mouseButton.y))
+				{
+					//we did
+					int range =(int)(signalTimerUpperLimit - signalTimeLowerLimit);
+					float signalSeconds = rand() % range + signalTimerUpperLimit;
+
+					timeTilsignal = sf::seconds(signalSeconds);
+				}
+			}
+
+
+
+
+			if (gameEvent.type == sf::Event::Closed)
 				gameWindow.close();
 		}
-		gameWindow.clear();
-		gameWindow.display();
-	}
+		
+		
+	
 	//------------------------------------------------
-	//------------------end inputs--------------------
+	//-----------------=End Inputs=-------------------
 	//------------------------------------------------
 
 
 	//----------------------------------------------
-	//------------------UPDATE=---------------------
+	//-----------------=UPDATE=---------------------
 	//----------------------------------------------
+	
 	//update time
 	sf::Time frameTime = gameClock.restart();
 
+	//update time till signal
+	timeTilsignal = timeTilsignal - frameTime;
+
+
+	if (timeTilsignal.asSeconds() <= 0.0f)
+	{
+		buttonSprite.setColor(sf::Color::Blue);
+
+	}
 	//----------------------------------------------
-	//-----------------end update--------------------
+	//----------------=End Update=-------------------
 	//------------------------------------------------
 
 
 	//------------------------------------------------
-	//-----------------draw---------------------------
+	//----------------=Draw=--------------------------
 	//------------------------------------------------
+
+	//clear the screen
+	gameWindow.clear(sf::Color::Cyan);
 
 	// Draw everything
 	gameWindow.draw(buttonSprite);
+	gameWindow.display();
 
 	//------------------------------------------------
-	//------------------end draw----------------------
+	//-----------------=end draw=---------------------
 	//-------------------------------------------------
+	}
 	return 0;
 }
